f35d98705b76be33b001c45192bc01ddfcfb09fb
gtg489w <gtg489w@gmail.com>
2016-01-02 18:27:00 -0800

    added audio hooks

--- web/app/scripts/controllers/main.js
+++ web/app/scripts/controllers/main.js
@@ -1,8 +1,39 @@
 'use strict';
-angular.module('canaryApp').controller('MainCtrl', function ($scope, $http, ApiService) {
+angular.module('canaryApp').controller('MainCtrl', function ($scope, $http, $timeout, ApiService) {
 
 	$scope.setPoints = function() {
 		ApiService.score.get();
+	};
+
+	$scope.audioPipeBurst = function() {
+		var audio = new Audio('audio/warning.wav');
+		audio.play();
+	};
+
+	$scope.audioDispatch = function() {
+		var audio = new Audio('audio/herewego.wav');
+		audio.play();
+
+		$timeout(function() {
+			var audio2 = new Audio('audio/pipe.wav');
+			audio2.play();
+
+			$timeout(function() {
+				var audio3 = new Audio('audio/underworld - cut.mp3');
+				audio3.play();
+			}, 1000);
+
+		}, 1000);
+	};
+
+	$scope.audioFix = function() {
+		var audio = new Audio('audio/powerup.wav');
+		audio.play();
+
+		$timeout(function() {
+			var audio2 = new Audio('audio/stageclear.wav');
+			audio2.play();
+		}, 1000);
 	};
 
 });
