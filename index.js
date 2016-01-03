e9be8c0f40659a21e60cfef323625a8a5990c548
Ken Bailey <kenneth.ross.bailey@gmail.com>
2016-01-03 04:11:13 -0800

    Adding hue lighting controls

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var request = require('request');
+
 var app = express();
 
 var leaks = {
@@ -18,8 +20,28 @@ app.post('/api/leaks', function (req, res) {
     console.log(req.body);
     if (req.body.value === 1) {
         leaks["pipe1"] = true;
+        request({
+            method: 'PUT',
+            uri: 'http://thompsonpaul.com:8080/api/36c997c030c1602f2ed53c1028e2281f/lights/4/state',
+            body: {
+                "on":true,
+                "sat":254,
+                "bri":254,
+                "hue":10000
+            }
+        });
     } else if (req.body.value === 0) {
         leaks["pipe1"] = false;
+        request({
+            method: 'PUT',
+            uri: 'http://thompsonpaul.com:8080/api/36c997c030c1602f2ed53c1028e2281f/lights/4/state',
+            body: {
+                "on":true,
+                "sat":254,
+                "bri":254,
+                "hue":30000
+            }
+        });
     }
     res.status(201).send();
 });
