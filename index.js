a491191168111361c338c7e1bc1340e3484359f4
EC2 Default User <ec2-user@ip-172-30-1-152.ec2.internal>
2016-01-03 13:01:50 +0000

    Final end-to-end integration.

--- index.js
+++ index.js
@@ -18,24 +18,24 @@ app.get('/api/leaks', function (req, res) {
 
 app.post('/api/leaks', function (req, res) {
     console.log(req.body);
-    if (req.body.value === 1) {
+    if (req.body.value == 1) {
         leaks["pipe1"] = true;
         request({
             method: 'PUT',
             uri: 'http://thompsonpaul.com:8080/api/36c997c030c1602f2ed53c1028e2281f/lights/4/state',
-            body: {
+            json: {
                 "on":true,
                 "sat":254,
                 "bri":254,
-                "hue":10000
+                "hue":1000
             }
         });
-    } else if (req.body.value === 0) {
+    } else if (req.body.value == 0) {
         leaks["pipe1"] = false;
         request({
             method: 'PUT',
             uri: 'http://thompsonpaul.com:8080/api/36c997c030c1602f2ed53c1028e2281f/lights/4/state',
-            body: {
+            json: {
                 "on":true,
                 "sat":254,
                 "bri":254,
@@ -43,6 +43,7 @@ app.post('/api/leaks', function (req, res) {
             }
         });
     }
+	console.log(leaks);
     res.status(201).send();
 });
 
